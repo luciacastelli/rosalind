de6ad7ce35db15bdf7731549974a4433f8a935f6
luciacastelli <lucia.castelli21@gmail.com>
2026-09-01 21:05:21 +0200

    Updated fib.py to implement botton-up dynamic programming

--- fib/fib.py
+++ fib/fib.py
@@ -1,15 +1,20 @@
 # Rabbits and recurrence relations
-from functools import lru_cache
 
-@lru_cache # memoization (dynamic programming)
-def fibonacci(n, k): # returns number of pairs at given month
+def fibonacci(n, k): # returns number of pairs (k) at given month (n)
     if n <= 2:
         return 1
-    else:
-        return (fibonacci(n - 1, k) +  k * fibonacci(n - 2, k))
+    
+    previous = 0
+    current = 1
+
+    for i in range(2, n + 1):
+        previous, current = current, previous * k + current
+        print(i, current)
+
+    return current
 
 def main():
-    print(fibonacci(36, 4))
+    print(fibonacci(6, 3))
 
 if __name__ == "__main__":
     main()
